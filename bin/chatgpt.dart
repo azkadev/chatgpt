02daae47315ae24bd99f48ee49f3ebb3eca50c66
￴ <>
2022-12-07 04:41:33 +0700

    first commit

---
+++ bin/chatgpt.dart
@@ -0,0 +1,5 @@
+import 'package:chatgpt/chatgpt.dart' as chatgpt;
+
+void main(List<String> arguments) {
+  print('Hello world: ${chatgpt.calculate()}!');
+}
---
+++ test/chatgpt_test.dart
@@ -0,0 +1,8 @@
+import 'package:chatgpt/chatgpt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
