06d840c970d5134e3fe8fcc47382915a33caeaeb
￴ <>
2022-12-07 04:45:40 +0700

    update

--- lib/chatgpt.dart
+++ lib/chatgpt.dart
@@ -1,3 +1,25 @@
+library chatgpt;
+
 int calculate() {
   return 6 * 7;
+}
+
+class ChatbotAccount {
+  late String email;
+  late String password;
+  ChatbotAccount({
+    required this.email,
+    required this.password,
+  });
+}
+
+class Chatbot {
+  late ChatbotAccount chatbotAccount;
+  Chatbot({
+    required this.chatbotAccount,
+  });
+
+  resetChat() {
+
+  }
 }
